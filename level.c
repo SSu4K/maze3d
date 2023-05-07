eea2d29b876c2682f754499949350e206bd50115
SSu4K <ssudak.kontak@gmail.com>
2023-05-07 15:34:56 +0200

    Added wall list and generation.

--- level.c
+++ level.c
@@ -8,6 +8,18 @@ static const SDL_Color BLUE = {0, 0, 255};
 static const SDL_Color DARK_BLUE = {0, 0, 150};
 static const SDL_Color DARK_VIOLET = {50, 0, 100};
 
+wall_t* new_wall(double x1, double y1, double x2, double y2, double height, SDL_Color color){
+    wall_t* wall = malloc(sizeof(wall_t));
+    wall->start_x=x1;
+    wall->start_y=y1;
+    wall->dx=x2-x1;
+    wall->dy=y2-y1;
+    wall->height=height;
+    wall->color=color;
+
+    return wall;
+}
+
 level_t* new_level(size_t map_width, size_t map_height, int block_width, int block_height){
     level_t* new_level=malloc(sizeof(level_t));
     new_level->map_width = map_width;
@@ -17,6 +29,8 @@ level_t* new_level(size_t map_width, size_t map_height, int block_width, int blo
     new_level->block_color_1=BLUE;
     new_level->block_color_2=DARK_BLUE;
     new_level->ground_color=DARK_VIOLET;
+    new_level->walls_count=0;
+    new_level->walls_list=calloc(0, sizeof(wall_t*));
 
     new_level->map = calloc(map_width, sizeof(int*));
     for(int x=0;x<map_width;x++){
@@ -37,6 +51,15 @@ level_t* new_rand_level(size_t map_width, size_t map_height, int block_width, in
         for(int y=0;y<map_width;y++){
             if(rand()%100<propability*100){
                 level->map[x][y]=1;
+                level->walls_count+=4;
+                level->walls_list = realloc(level->walls_list, level->walls_count*sizeof(wall_t*));
+                double width = level->block_width;
+                double start_x=x*width;
+                double start_y=y*width;
+                level->walls_list[level->walls_count-1]=new_wall(start_x, start_y, start_x+width, start_y, level->block_height, DARK_BLUE);
+                level->walls_list[level->walls_count-2]=new_wall(start_x, start_y+width, start_x+width, start_y+width, level->block_height, DARK_BLUE);
+                level->walls_list[level->walls_count-3]=new_wall(start_x, start_y, start_x, start_y+width, level->block_height, BLUE);
+                level->walls_list[level->walls_count-4]=new_wall(start_x+width, start_y, start_x+width, start_y+width, level->block_height, BLUE);
             }
         }    
     }
@@ -44,9 +67,17 @@ level_t* new_rand_level(size_t map_width, size_t map_height, int block_width, in
 }
 
 void free_level(level_t* level){
+
     for(int x=0;x<level->map_width;x++){
         free(level->map[x]);    
     }
+    for(int i=0;i<level->walls_count;i++){
+        if(level->walls_list[i]!=NULL){
+            free(level->walls_list[i]);
+        }
+    }
+    
+    free(level->walls_list);
     free(level->map);
     free(level);
 }
--- level.h
+++ level.h
@@ -3,7 +3,17 @@
 #define __LEVEL_H__
 
 #include <SDL2/SDL.h>
-#include<stdbool.h>
+#include <stdbool.h>
+
+typedef struct Wall
+{
+    double start_x;
+    double start_y;
+    double dx;
+    double dy;
+    double height;
+    SDL_Color color;
+} wall_t;
 
 typedef struct Level
 {
@@ -14,13 +24,15 @@ typedef struct Level
     SDL_Color block_color_1;
     SDL_Color block_color_2;
     SDL_Color ground_color;
+    wall_t** walls_list;
+    size_t walls_count;
     int **map;
 } level_t;
 
-level_t* new_level(size_t map_width, size_t map_height, int block_width, int block_height);
-level_t* new_rand_level(size_t map_width, size_t map_height, int block_width, int block_height, float propability);
-void free_level(level_t* level);
-bool is_in_bound(double x, double y, level_t* level);
-int get_block_color(double x, double y, level_t* level);
+level_t *new_level(size_t map_width, size_t map_height, int block_width, int block_height);
+level_t *new_rand_level(size_t map_width, size_t map_height, int block_width, int block_height, float propability);
+void free_level(level_t *level);
+bool is_in_bound(double x, double y, level_t *level);
+int get_block_color(double x, double y, level_t *level);
 
 #endif
